d47cd0d8ff5462e09623bd5c5a52244f7abd2050
topottop <burhanovtimur047@gmail.com>
2025-05-17 23:12:10 +0500

    uty

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-  let points = 0;
-  let pointGrowth = 1;
-  let ticks=24;
-  myTimer = setInterval(endOfTurnCalc, 1000/ticks);
-  function endOfTurnCalc() {
-    points = points+pointGrowth/ticks;
-    document.getElementById("point_amount_string").innerHTML = points.toFixed(2);
-    document.getElementById("point_amount").innerHTML = points;
-  }
+var points = 0;
+var pointGrowth = 1;
+var lastUpdate = Date.now()
+
+
+function format(amount) {
+  let power = Math.floor(Math.log10(amount))
+  let mantissa = amount / Math.pow(10, power)
+  if (power < 3) return amount.toFixed(2)
+  return mantissa.toFixed(2) + "e" + power
+}
+
+function TestBoost() {
+  pointGrowth *= 2
+  pointGrowth **= 2
+}
+
+function updateGUI() {
+  document.getElementById("points").textContent = format(points);
+  document.getElementById("pointGrowth").textContent = 'You gain ' + format(pointGrowth) + ' points per second.';
+}
+
+function productionLoop(diff) {
+  points += pointGrowth * diff
+}
+
+function mainLoop() {
+  var diff = (Date.now() - lastUpdate) / 1000
+
+  productionLoop(diff)
+  updateGUI()
+
+  lastUpdate = Date.now()
+}
+
+setInterval(mainLoop, 50)
